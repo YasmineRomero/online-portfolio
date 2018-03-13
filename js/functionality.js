15a599013dd9eda5da83f9f2e23dd4d14e9aaa9e
Romero Yasmine <310045010@cart.ads>
2018-03-13 15:30:42 -0700

    fix everything past About Me

--- js/functionality.js
+++ js/functionality.js
@@ -1,3 +1,5 @@
+// For Dropdown Navigation \\
+
 var c = false;
 
 function showPortfolio() {
@@ -10,3 +12,49 @@ function showPortfolio() {
     }
 
 }
+
+
+// Index: Scroll to Introduction \\
+
+var reveal = document.getElementById("index-parallax");
+reveal.scrollIntoView();
+
+
+// Form Validation \\
+
+
+// Slideshow \\
+
+
+
+function slideShow(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    
+    if (n > slides.length) {slideIndex = 1} 
+    if (n < 1) {slideIndex = slides.length}
+    
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none"; 
+    }
+    slides[slideIndex-1].style.display = "block"; 
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function changeSlide(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+
+
+
+// > "plusSlide(1)"
+// < "plusSlide(-1)"
