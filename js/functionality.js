b3cab0af341015d500df6c40a59fbca957dd3cf1
Romero Yasmine <310045010@cart.ads>
2018-03-20 13:07:24 -0700

    Eh

--- js/functionality.js
+++ js/functionality.js
@@ -13,40 +13,6 @@ function showPortfolio() {
 
 }
 
-// Slideshow \\
-
-var slideIndex = 1;
-showDivs(slideIndex);
-
-function plusDivs(n) {
-    showDivs(slideIndex += n);
-}
-
- function currentDiv(n) {
-    showDivs(slideIndex = n);
-}
-
-function showDivs(n) {
-    var i;
-    var x = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("demo");
-    
-    if (n > x.length) {
-        slideIndex = 1;
-    }    
-    if (n < 1) {
-        slideIndex = x.length;
-    }
-    for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none";  
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace("slide-red", "");
-    }
-    x[slideIndex-1].style.display = "block";  
-    dots[slideIndex-1].className += "slide-red";
-}
-
 // Form Validation \\
 
 function validateEmail() {
