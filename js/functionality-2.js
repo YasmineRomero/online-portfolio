726c54b696b5ecee313790e5aa497a89a72bfe6a
Romero Yasmine <310045010@cart.ads>
2018-03-22 15:31:00 -0700

    jjjj

--- js/functionality-2.js
+++ js/functionality-2.js
@@ -13,19 +13,10 @@ function showPortfolio() {
 
 }
 
-// Form Validation \\
+// Form Validation (work on l8r) \\
 
 function validateEmail() {
     var email = document.forms[0][0].value;
-    
-    if (email == "") {
-        alert("Please put in an e-mail.");
-        return false;
-    }
-    else if (email.search("@") !== 0) {
-        alert("Please put in a valid e-mail.");
-        return false;
-    }
 }
 
 // Slideshow \\
